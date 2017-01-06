b01c1e2a27d34872ece5aaf39304127fcfa39b84
Alex Costantini <alexcostantini@alexcostantini.fios-router.home>
2017-01-05 21:39:29 -0500

    removed supplier from product model

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,17 +1,15 @@
 Spree::Product.class_eval do
-  after_create :set_supplier
 
   has_many :suppliers, through: :master
-  belongs_to :supplier
 
   def add_supplier!(supplier_or_id)
-    splr = supplier_or_id.is_a?(Spree::Supplier) ? supplier_or_id : Spree::Supplier.find(supplier_or_id)
-    populate_for_supplier! splr if splr
+    supplier = supplier_or_id.is_a?(Spree::Supplier) ? supplier_or_id : Spree::Supplier.find(supplier_or_id)
+    populate_for_supplier! supplier if supplier
   end
 
   def add_suppliers!(supplier_ids)
-    Spree::Supplier.where(id: supplier_ids).each do |splr|
-      populate_for_supplier! splr
+    Spree::Supplier.where(id: supplier_ids).each do |supplier|
+      populate_for_supplier! supplier
     end
   end
 
@@ -29,10 +27,6 @@ Spree::Product.class_eval do
         supplier.stock_locations.each { |location| location.propagate_variant(variant) }
       end
     end
-  end
-
-  def set_supplier
-    add_supplier! self.supplier
   end
 
 end
