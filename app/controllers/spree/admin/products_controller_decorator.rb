352b70562b6c13ba0362ea2e464114048f693a2f
Alex Costantini <alexcostantini@alexcostantini.local>
2017-01-19 16:45:17 -0500

    removed with_deleted from supplier product scope

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -13,14 +13,6 @@ Spree::Admin::ProductsController.class_eval do
   def supplier_collection
     if try_spree_current_user && !try_spree_current_user.admin? && try_spree_current_user.supplier?
       @collection = @collection.joins(:suppliers).where('spree_suppliers.id = ?', try_spree_current_user.supplier_id)
-    
-      params[:q] ||= {}
-      params[:q][:deleted_at_null] ||= "1"
-      params[:q][:not_discontinued] ||= "1"
-
-      if params[:q][:deleted_at_null] == '0'
-          @collection = @collection.with_deleted
-      end
     end
   end
 
