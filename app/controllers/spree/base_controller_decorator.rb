187aed34795c2ec845fd29165a90aa997bd1b012
Alex Costantini <alexcostantini@alexcostantini.fios-router.home>
2017-03-08 15:34:18 -0500

    wip

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -1,13 +1,27 @@
 Spree::BaseController.class_eval do
 
-  prepend_before_filter :redirect_supplier
+	prepend_before_filter :redirect_supplier
 
-  private
+	private
 
-  def redirect_supplier
-    if ['/admin', '/admin/authorization_failure'].include?(request.path) && try_spree_current_user.try(:supplier)
-      redirect_to '/admin/shipments' and return false
-    end
-  end
+	def redirect_supplier
+		if ['/admin', '/admin/authorization_failure'].include?(request.path) && try_spree_current_user.try(:supplier)
+		  redirect_to '/admin/shipments' and return false
+		end
+	end
+
+	def redirect_unauthorized_access
+		if try_spree_current_user
+			# flash[:error] = Spree.t(:authorization_failure)
+			redirect_to spree.forbidden_path
+		else
+			store_location
+			if respond_to?(:spree_login_path)
+			  redirect_to spree_login_path
+			else
+			  redirect_to spree.respond_to?(:root_path) ? spree.root_path : main_app.root_path
+			end
+		end
+	end
 
 end
