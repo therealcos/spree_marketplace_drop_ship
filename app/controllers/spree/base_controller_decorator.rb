1dac32f5c4eab96a4f1cd6b55e2c138bd1f3aa10
Alex Costantini <alexcostantini@alexcostantini.fios-router.home>
2017-03-08 15:50:19 -0500

    wip

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -10,18 +10,4 @@ Spree::BaseController.class_eval do
 		end
 	end
 
-	def redirect_unauthorized_access
-		if try_spree_current_user
-			# flash[:error] = Spree.t(:authorization_failure)
-			redirect_to spree.forbidden_path
-		else
-			store_location
-			if respond_to?(:spree_login_path)
-			  redirect_to spree_login_path
-			else
-			  redirect_to spree.respond_to?(:root_path) ? spree.root_path : main_app.root_path
-			end
-		end
-	end
-
 end
